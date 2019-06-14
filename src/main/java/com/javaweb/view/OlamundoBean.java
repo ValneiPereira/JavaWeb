f729198c6607818ab7d3bad1ca3aaebd33115ec4
ValneiPereira <valnney@yahoo.com.br>
2019-06-14 14:05:50 -0300

    atualizando

---
+++ src/main/java/com/javaweb/view/OlamundoBean.java
@@ -0,0 +1,35 @@
+package com.javaweb.view;
+
+import java.sql.Date;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlamundoBean {
+  private String nome;
+  private String email;
+  private Date dataNascimento;
+  
+  
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public Date getDataNascimento() {
+    return dataNascimento;
+  }
+  public void setDataNascimento(Date dataNascimento) {
+    this.dataNascimento = dataNascimento;
+  }
+
+  
+
+}
